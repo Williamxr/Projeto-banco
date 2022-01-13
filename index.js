ca519f4d73efdfda5863414a9e00c109af10ba6f
William <william.teletabs@hotmail.com>
2022-01-13 16:22:35 -0300

    Atribuindo parâmetros dos construtores

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 133322211109);
 
-cliente1.nome = "Ricardo";
-cliente1.cpf = "11122233309";
-
-
-cliente2.nome = "Alice"
-cliente2.cpf = "11122233309";
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 
 
@@ -22,11 +13,10 @@ console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(cliente2, 1002);
 console.log(conta2);
 
-contaCorrenteRicardo.transferir(200, conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 console.log(conta2.saldo);
 console.log(contaCorrenteRicardo);
